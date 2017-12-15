99d74fcff119eeff57eb7b00d3ebef53c84ffeb4
kmchao33 <kmchao33@gmail.com>
2017-12-15 16:07:57 +0100

    Add files via upload

---
+++ libs/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/ChuiGum/Documents/libs/glfw/src/clipboard.c" "/Users/ChuiGum/Documents/libs/glfw/src/CMakeFiles/glfw.dir/clipboard.c.o"
+  "/Users/ChuiGum/Documents/libs/glfw/src/cocoa_clipboard.m" "/Users/ChuiGum/Documents/libs/glfw/src/CMakeFiles/glfw.dir/cocoa_clipboard.m.o"
+  "/Users/ChuiGum/Documents/libs/glfw/src/cocoa_gamma.c" "/Users/ChuiGum/Documents/libs/glfw/src/CMakeFiles/glfw.dir/cocoa_gamma.c.o"
+  "/Users/ChuiGum/Documents/libs/glfw/src/cocoa_init.m" "/Users/ChuiGum/Documents/libs/glfw/src/CMakeFiles/glfw.dir/cocoa_init.m.o"
+  "/Users/ChuiGum/Documents/libs/glfw/src/cocoa_joystick.m" "/Users/ChuiGum/Documents/libs/glfw/src/CMakeFiles/glfw.dir/cocoa_joystick.m.o"
+  "/Users/ChuiGum/Documents/libs/glfw/src/cocoa_monitor.m" "/Users/ChuiGum/Documents/libs/glfw/src/CMakeFiles/glfw.dir/cocoa_monitor.m.o"
+  "/Users/ChuiGum/Documents/libs/glfw/src/cocoa_time.c" "/Users/ChuiGum/Documents/libs/glfw/src/CMakeFiles/glfw.dir/cocoa_time.c.o"
+  "/Users/ChuiGum/Documents/libs/glfw/src/cocoa_window.m" "/Users/ChuiGum/Documents/libs/glfw/src/CMakeFiles/glfw.dir/cocoa_window.m.o"
+  "/Users/ChuiGum/Documents/libs/glfw/src/context.c" "/Users/ChuiGum/Documents/libs/glfw/src/CMakeFiles/glfw.dir/context.c.o"
+  "/Users/ChuiGum/Documents/libs/glfw/src/gamma.c" "/Users/ChuiGum/Documents/libs/glfw/src/CMakeFiles/glfw.dir/gamma.c.o"
+  "/Users/ChuiGum/Documents/libs/glfw/src/init.c" "/Users/ChuiGum/Documents/libs/glfw/src/CMakeFiles/glfw.dir/init.c.o"
+  "/Users/ChuiGum/Documents/libs/glfw/src/input.c" "/Users/ChuiGum/Documents/libs/glfw/src/CMakeFiles/glfw.dir/input.c.o"
+  "/Users/ChuiGum/Documents/libs/glfw/src/joystick.c" "/Users/ChuiGum/Documents/libs/glfw/src/CMakeFiles/glfw.dir/joystick.c.o"
+  "/Users/ChuiGum/Documents/libs/glfw/src/monitor.c" "/Users/ChuiGum/Documents/libs/glfw/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/Users/ChuiGum/Documents/libs/glfw/src/nsgl_context.m" "/Users/ChuiGum/Documents/libs/glfw/src/CMakeFiles/glfw.dir/nsgl_context.m.o"
+  "/Users/ChuiGum/Documents/libs/glfw/src/time.c" "/Users/ChuiGum/Documents/libs/glfw/src/CMakeFiles/glfw.dir/time.c.o"
+  "/Users/ChuiGum/Documents/libs/glfw/src/window.c" "/Users/ChuiGum/Documents/libs/glfw/src/CMakeFiles/glfw.dir/window.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
